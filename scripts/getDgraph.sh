0a1e10c8aab3e61b8a233aab326532ab1138616b
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2018-05-07 22:30:07 +1000

    bump to Dgraph v1.0.5

--- scripts/getDgraph.sh
+++ scripts/getDgraph.sh
@@ -4,10 +4,12 @@
 
 nuget install Grpc.Tools -Version 1.7.1 -OutputDirectory artifacts
 
-git clone --branch v1.0.4 --depth 1 https://github.com/dgraph-io/dgraph artifacts/dgraph
+git clone --branch v1.0.5 --depth 1 https://github.com/dgraph-io/dgraph artifacts/dgraph
+git clone --branch master --depth 1 https://github.com/dgraph-io/dgo artifacts/dgo
 
-artifacts/Grpc.Tools.1.7.1/tools/macosx_x64/protoc -I artifacts/dgraph/protos \
+echo "Building C# protos files"
+
+artifacts/Grpc.Tools.1.7.1/tools/macosx_x64/protoc -I artifacts/dgraph/protos -I artifacts/dgo/protos \
     --csharp_out source/Dgraph-dotnet/DgraphAPI \
-    --grpc_out source/Dgraph-dotnet/DgraphAPI artifacts/dgraph/protos/*.proto \
-    --plugin=protoc-gen-grpc=artifacts/Grpc.Tools.1.7.1/tools/macosx_x64/grpc_csharp_plugin
-
+    --grpc_out source/Dgraph-dotnet/DgraphAPI artifacts/dgraph/protos/*.proto artifacts/dgo/protos/*.proto \
+    --plugin=protoc-gen-grpc=artifacts/Grpc.Tools.1.7.1/tools/maco
