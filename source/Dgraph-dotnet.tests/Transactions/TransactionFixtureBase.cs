ea27691a0d88dbfb7637469455f5272799b0d3da
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-03-19 23:25:50 +1100

    base for DiscardFixture

--- source/Dgraph-dotnet.tests/Transactions/TransactionFixtureBase.cs
+++ source/Dgraph-dotnet.tests/Transactions/TransactionFixtureBase.cs
@@ -16,5 +16,17 @@ namespace Dgraph_dotnet.tests.Transactions {
 
             return (client, response);
         }
+
+        internal (IDgraphClientInternal, Assigned) MinimalClientForMutation() {
+            var client = Substitute.For<IDgraphClientInternal>();
+            var linRead = new LinRead();
+            client.GetLinRead().Returns(linRead);
+
+            var assigned = new Assigned();
+            assigned.Context = new TxnContext();;
+            client.Mutate(Arg.Any<Api.Mutation>()).Returns(assigned);
+
+            return (client, assigned);
+        }
     }
 }
