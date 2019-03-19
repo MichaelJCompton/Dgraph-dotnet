f7548e888a35e8f3c5c4bab703ed35e4898df0bf
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-03-19 22:44:44 +1100

    add tests about transaction errors

--- source/Dgraph-dotnet.tests/Transactions/TransactionFixture.cs
+++ source/Dgraph-dotnet.tests/Transactions/TransactionFixture.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 
 namespace Dgraph_dotnet.tests.Transactions {
-    public class TransactionFixture {
+    public class TransactionFixture : TransactionFixtureBase {
 
         // 
         // ------------------------------------------------------
@@ -44,10 +45,73 @@ namespace Dgraph_dotnet.tests.Transactions {
         // ------------------------------------------------------
         //
 
-        public void All_ExceptionIfDiscarded() {
-            // grab everything from the interfaces
-            // discard the transaction
-            // loop through, should get an exception for each
+        [Test]
+        public void All_FailIfAlreadyCommitted() {
+            var client = Substitute.For<IDgraphClientInternal>();
+            var txn = new Transaction(client);
+            txn.Commit();
+
+            var tests = GetAllTestFunctions(txn);
+
+            foreach (var test in tests) {
+                var result = test();
+                result.IsFailed.Should().BeTrue();
+            }
         }
+
+        [Test]
+        public void All_FailIfDiscarded() {
+            var client = Substitute.For<IDgraphClientInternal>();
+            var txn = new Transaction(client);
+            txn.Discard();
+
+            var tests = GetAllTestFunctions(txn);
+
+            foreach (var test in tests) {
+                var result = test();
+                result.IsFailed.Should().BeTrue();
+            }
+        }
+
+        [Test]
+        public void All_ExceptionIfDisposed() {
+            var client = Substitute.For<IDgraphClientInternal>();
+            var txn = new Transaction(client);
+            txn.Dispose();
+
+            var tests = GetAllTestFunctions(txn);
+
+            foreach (var test in tests) {
+                test.Should().Throw<ObjectDisposedException>();
+            }
+        }
+
+        [Test]
+        public void All_FailIfTransactionError() {
+            // force transaction into error state
+            var client = Substitute.For<IDgraphClientInternal>();
+            client.Mutate(Arg.Any<Api.Mutation>()).Throws(new RpcException(new Status(), "Something failed"));
+            var txn = new Transaction(client);
+            
+            txn.Mutate("{ }");
+
+            var tests = GetAllTestFunctions(txn);
+
+            foreach (var test in tests) {
+                var result = test();
+                result.IsFailed.Should().BeTrue();
+            }
+        }
+
+        private List<Func<ResultBase>> GetAllTestFunctions(Transaction txn) =>
+            new List<Func<ResultBase>> {
+                () => txn.Commit(),
+                () => txn.Delete(""),
+                () => txn.Mutate(""),
+                () => txn.Query(""),
+                () => txn.QueryWithVars("", null),
+                () => txn.SchemaQuery(),
+                () => txn.SchemaQuery("")
+            };
     }
 }
