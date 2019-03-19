21e7ccea6d8102123c0ad8a97cccc2df56f58d15
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-03-19 12:18:11 +1100

    Add schema string output

--- source/Dgraph-dotnet/DgraphSchema/DgraphSchema.cs
+++ source/Dgraph-dotnet/DgraphSchema/DgraphSchema.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DgraphDotNet.Schema {
 
     public class DgraphSchema {
         public List<DrgaphPredicate> Schema { get; set; }
+
+        public override string ToString() =>
+            string.Join("\n", Schema.Select(p => p.ToString()));
     }
 }
